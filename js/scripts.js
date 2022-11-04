2bf726e629a8ca2b78e78f518fee5672e80372a2
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-11-04 09:52:56 -0700

    implemented calculate cost

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,38 @@ function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size;
 }
+
+Pizza.prototype.calculateCost = function () {
+  let cost = 0;
+  switch(this.toppings.length){
+    case 0:
+      cost -= 5;
+      break;
+    case 1:
+      cost -= 2;
+      break;
+    case 2:
+      cost += 1;
+      break;
+    case 3:
+      cost += 5;
+      break;
+    case 4:
+      cost += 7;
+      break;
+    default:
+      cost += 10;
+  }
+
+  switch(this.size){
+    case "small":
+      cost += 10;
+      break;
+    case "medium":
+      cost += 15;
+      break;
+    case "large":
+      cost += 20;
+  }
+  return cost;
+}
